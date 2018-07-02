8bacb01ca30c890a71d291c0b267a43ffda9769a
zekym <yinshunlin@saicmotor.com>
2018-07-02 10:47:17 +0800

    u

--- mqtt/emq/emq-client1/src/main/java/com/test/mqtt/emq/controller/ProducerController.java
+++ mqtt/emq/emq-client1/src/main/java/com/test/mqtt/emq/controller/ProducerController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,8 @@ import com.test.mqtt.emq.conf.MqttConfiguration.MyGateway;
 
 @RestController
 public class ProducerController {
+	
+	private static Logger logger = LogManager.getLogger(ProducerController.class);
 
 	@Autowired
 	private MyGateway gateway;
@@ -20,7 +24,9 @@ public class ProducerController {
 	public void send(String topic, String msg) {
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		String payload = "foo-" + df.format(new Date());
-		System.out.println("topic: " + topic + ", payload: " + payload);
+		String message = "topic: " + topic + ", payload: " + payload;
+		System.out.println(message);
+		logger.info(message);
 		gateway.sendToMqtt(payload, topic);
 	}
 }
